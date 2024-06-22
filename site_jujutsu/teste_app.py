00e59ce3bae2113202941c38572a4cf755c149c8
HenriqueAlmeidaLucareli <henrique.lucareli@germinare.org.br>
2024-06-22 10:20:18 -0300

    V8 DEV

---
+++ site_jujutsu/teste_app.py
@@ -0,0 +1,16 @@
+import unittest
+from site_jujutsu.app import app
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello, World!", response.data)  # Ajuste conforme o conteúdo do index.html
+
+if __name__ == "__main__":
+    unittest.main()
